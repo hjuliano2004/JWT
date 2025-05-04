adb3e0fa64f91b903e6be7fa1d0dd1dc3db5a9d4
hjuliano2004 <112650184+hjuliano2004@users.noreply.github.com>
2025-05-04 16:12:48 -0300

    pacote do postman

--- projeto/ccc.sql
+++ projeto/ccc.sql
@@ -1,4 +1,4 @@
--- Active: 1744137910213@@127.0.0.1@5432@organizacao@public
+-- Active: 1746308139650@@dpg-d0b7sap5pdvs73ceoc70-a.oregon-postgres.render.com@5432@organizacao@public
 DROP TABLE usuarios;
 
 select * from usuarios;  
